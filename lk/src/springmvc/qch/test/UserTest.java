4433676f55bf8e9d2085977ec2ea6d68e7e8b097
quanchanghong <1126381399@qq.com>
2017-11-21 21:33:46 +0800

    spring使用4.3.9版本，dao配置C3P0基本完成

--- lk/src/springmvc/qch/test/UserTest.java
+++ lk/src/springmvc/qch/test/UserTest.java
@@ -1,16 +1,17 @@
 package springmvc.qch.test;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-public class UserTest extends JdbcDaoSupport{
+public class UserTest {
 
-	public void run(){
-		JdbcTemplate jdbcTemplate2 = this.getJdbcTemplate();
-		
+	@Test
+	public void testLogin(){
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("/config/spring_dao.xml");
+		JdbcTemplate jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+		jdbcTemplate.execute("insert into user(userId, username, password) values(3,'testjdbc','hello')");
 	}
 	
 	
